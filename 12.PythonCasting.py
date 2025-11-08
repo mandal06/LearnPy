cba30197e35d004d59bfc8483bbcfeb18466abd2
mandal06 <adritamandal88@gmail.com>
2025-11-08 13:06:59 +0600

    uploading

---
+++ 12.PythonCasting.py
@@ -0,0 +1,30 @@
+print("---types of int---")
+
+a = int(1)
+b = int(5.22)
+c = int("10")
+
+print(a)  # Output: 1
+print(b)  # Output: 5
+print(c)  # Output: 10
+
+print("\n")
+print("---types of float---")
+
+x = float(1)
+y = float(5.22)
+z = float("10")
+
+print(x)  # Output: 1
+print(y)  # Output: 5
+print(z)  # Output: 10
+
+print("\n")
+print("---types of String---")
+r = str(1)
+q = str(5.22)
+p = str("10")
+
+print(r)  # Output: 1
+print(q)  # Output: 5
+print(p)  # Output: 10
