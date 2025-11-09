6c7c458614ac57edaada3e5f0dd42bdf472b5009
mandal06 <adritamandal88@gmail.com>
2025-11-10 00:42:45 +0600

    Loading

---
+++ 13.PythonStrings.py
@@ -0,0 +1,13 @@
+print("Hello")
+print("World")
+
+#others examples of string creation
+a = 'Hello'
+print(a) 
+
+b = """My name is Palash Mandal.
+I am a student is University of Global Village (UGV), Barisal.
+Department of Computer Science and Engineering (CSE).
+I like Football very much.
+"""
+print(b)
---
+++ 14.SlicingStrings.py
@@ -0,0 +1,17 @@
+a = "hello, world"
+print(a[2:5])      # Output: llo
+
+#next example
+
+b =  "hello, world"
+print(b[:5])       # Output: hello
+
+#next example
+
+b =  "hello, world"
+print(b[2:])  
+
+#next example
+
+b =  "hello, world"
+print(b[-5:-2]) 
