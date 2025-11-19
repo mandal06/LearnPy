33ca665664a78e0961837979c81ac99850752541
mandal06 <adritamandal88@gmail.com>
2025-11-19 20:15:42 +0600

    Update

--- 13.PythonStrings.py
+++ 13.PythonStrings.py
@@ -5,6 +5,8 @@ print("World")
 a = 'Hello'
 print(a) 
 
+#others examples of string creation
+
 b = """My name is Palash Mandal.
 I am a student is University of Global Village (UGV), Barisal.
 Department of Computer Science and Engineering (CSE).
---
+++ 16.ConcatenatedStrings.py
@@ -0,0 +1,11 @@
+a = "Palash"
+b = " Mandal"
+c = a + b
+print(c)  # Output: Palash Mandal
+
+print("/n")
+
+d = "mandal"
+f = "is a student."
+e = d + " " + f
+print(e)  # Output: Palash Mandal mandal
